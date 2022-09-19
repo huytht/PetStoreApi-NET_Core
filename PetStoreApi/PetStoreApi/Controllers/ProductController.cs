e1dacee6319299e8e2a92c42cb5da6f805b0e459
huytht <huytht2001@gmail.com>
2022-09-20 02:07:31 +0700

    update product controller

---
+++ PetStoreApi/PetStoreApi/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PetStoreApi.DTO.ProductDTO;
+using PetStoreApi.Domain;
+using PetStoreApi.Services;
+
+namespace PetStoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct([FromForm] ProductCreateDto product)
+        {
+            Task<AppServiceResult<ProductDto>> result = _productRepository.AddProduct(product);
+            Console.WriteLine(result);
+            return Ok(result);
+        }
+    }
+}
