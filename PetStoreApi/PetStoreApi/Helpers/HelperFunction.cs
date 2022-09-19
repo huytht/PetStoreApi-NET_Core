0939cff8feb915700b5e8263138f2967936ce0e9
huytht <huytht2001@gmail.com>
2022-09-20 02:06:24 +0700

    update helper function

---
+++ PetStoreApi/PetStoreApi/Helpers/HelperFunction.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace PetStoreApi.Helpers
+{
+    public class HelperFunction
+    {
+        public static string stripAccents(string s)
+        {
+            s = s.Normalize(NormalizationForm.FormD);
+            s = s.Replace("[\\p{InCombiningDiacriticalMarks}]", "");
+            return s;
+        }
+
+        public static string normalizeUri(string input)
+        {
+            input = input.Replace("đ", "d").Replace("Đ", "D");
+
+            input = stripAccents(input);
+
+            return input.Trim().Replace("[^a-zA-Z0-9]+", "-").ToLower();
+        }
+    }
+}
