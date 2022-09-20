90ef1555af3afb2cf848f7096738011572bfcded
huytht <huytht2001@gmail.com>
2022-09-21 02:00:02 +0700

    update api for call image

---
+++ PetStoreApi/PetStoreApi/Controllers/MyFileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace PetStoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/myfile")]
+    public class MyFileController : Controller
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public MyFileController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("images/{fileName}")]
+        public FileResult getImageFile(string fileName)
+        {
+            var path = Path.Combine("", _hostingEnvironment.ContentRootPath + "/Images/" + fileName);
+
+             return File(System.IO.File.ReadAllBytes(path), "image/png");
+        }
+    }
+}
