eee04344cad00555a22d9d4bca3931675b4b797d
huytht <huytht2001@gmail.com>
2022-09-21 02:03:10 +0700

    update product list api

--- PetStoreApi/PetStoreApi/Controllers/ProductController.cs
+++ PetStoreApi/PetStoreApi/Controllers/ProductController.cs
@@ -3,25 +3,39 @@ using Newtonsoft.Json;
 using PetStoreApi.DTO.ProductDTO;
 using PetStoreApi.Domain;
 using PetStoreApi.Services;
+using PetStoreApi.Data.Entity;
 
 namespace PetStoreApi.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/product")]
     public class ProductController : Controller
     {
-        private IProductRepository _productRepository;
+        private readonly IProductRepository _productRepository;
 
         public ProductController(IProductRepository productRepository)
         {
             _productRepository = productRepository;
         }
+        [HttpGet("cat/list")]
+        public IActionResult GetCatList()
+        {
+            AppServiceResult<List<ProductShortDto>> result = _productRepository.GetCatList();
 
+            return Ok(result);
+        }
+        [HttpGet("dog/list")]
+        public IActionResult GetDogList()
+        {
+            AppServiceResult<List<ProductShortDto>> result = _productRepository.GetDogList();
+
+            return Ok(result);
+        }
         [HttpPost]
         public IActionResult AddProduct([FromForm] ProductCreateDto product)
         {
-            Task<AppServiceResult<ProductDto>> result = _productRepository.AddProduct(product);
-            Console.WriteLine(result);
+            AppServiceResult<Product> result = _productRepository.AddProduct(product);
+            Console.WriteLine("=========================================>" + result);
             return Ok(result);
         }
     }
