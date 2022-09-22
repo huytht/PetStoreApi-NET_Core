e64c103f919553a605379c1517a2d6b7b1b18a42
huytht <huytht2001@gmail.com>
2022-09-23 00:20:10 +0700

    update param and pagination

---
+++ PetStoreApi/PetStoreApi/Domain/FilterParam.cs
@@ -0,0 +1,17 @@
+﻿namespace PetStoreApi.Domain
+{
+    public class FilterParam
+    {
+        public int BreedId { get; set; }
+        public int CategoryId { get; set; }
+
+        public FilterParam()
+        {
+        }
+        public FilterParam(int breedId, int categoryId)
+        {
+            BreedId = breedId;
+            CategoryId = categoryId;
+        }
+    }
+}
---
+++ PetStoreApi/PetStoreApi/Domain/PageParam.cs
@@ -0,0 +1,17 @@
+﻿namespace PetStoreApi.Domain
+{
+    public class PageParam
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+
+        public PageParam()
+        {
+        }
+        public PageParam(int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+        }
+    }
+}
