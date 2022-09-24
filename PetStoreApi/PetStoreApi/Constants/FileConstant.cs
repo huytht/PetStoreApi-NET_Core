af9ab3efe56fef21a0795b1acbf8f508daac8c67
huytht <huytht2001@gmail.com>
2022-09-25 00:36:00 +0700

    update file and role constant

--- PetStoreApi/PetStoreApi/Constants/FileConstant.cs
+++ PetStoreApi/PetStoreApi/Constants/FileConstant.cs
@@ -3,5 +3,6 @@
     public class FileConstant
     {
         public const string FILE_URL_PATH = "/myfile/images/";
+        public const string TEMP_PROFILE_IMAGE_BASE_URL = "https://robohash.org/";
     }
 }
---
+++ PetStoreApi/PetStoreApi/Constants/RoleConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace PetStoreApi.Constants
+{
+    public class RoleConstant
+    {
+        public static string ROLE_MEMBER = "ROLE_MEMBER";
+        public static string ROLE_ADMIN = "ROLE_ADMIN";
+    }
+}
