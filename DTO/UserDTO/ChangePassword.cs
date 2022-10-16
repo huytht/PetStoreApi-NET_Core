bac1977a4b30dc2531bb88ccac8f1a69a575f9e3
huytht <huytht2001@gmail.com>
2022-10-17 00:01:27 +0700

    update validate for login and change password

--- DTO/UserDTO/ChangePassword.cs
+++ DTO/UserDTO/ChangePassword.cs
@@ -1,9 +1,16 @@
-﻿namespace PetStoreApi.DTO.UserDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetStoreApi.DTO.UserDTO
 {
     public class ChangePassword
     {
+        [Required]
+        [MinLength(length: 2, ErrorMessage = "username should have at least 2 characters")]
         public string Username { get; set; }
+        [Required]
         public string OldPassword { get; set; }
+        [Required]
+        [MinLength(length: 8, ErrorMessage = "password should have at least 8 characters")]
         public string NewPassword { get; set; }
         public ChangePassword()
         {
--- DTO/UserDTO/UserLoginDto.cs
+++ DTO/UserDTO/UserLoginDto.cs
@@ -5,10 +5,10 @@ namespace PetStoreApi.DTO.UserDTO
     public class UserLoginDto
     {
         [Required]
-        [MaxLength(50)]
+        [MinLength(length: 2, ErrorMessage = "username should have at least 2 characters")]
         public string Username { get; set; }
         [Required]
-        [MaxLength(50)]
+        [MinLength(length: 8, ErrorMessage = "password should have at least 8 characters")]
         public string Password { get; set; }
         public UserLoginDto()
         {
